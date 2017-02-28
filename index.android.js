962e90fa92fafe265d5fd372a6ea383c7fe9817d
jkomyno <skiabo97@gmail.com>
2017-02-28 15:43:53 +0100

    Generated a new RN project and copied AirBnb's react-native-maps' examples in

---
+++ index.android.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+class reactNativeMapsExample extends React.Component {
+  render() {
+    return <App />;
+  }
+}
+
+AppRegistry.registerComponent('reactNativeMapsExample', () => reactNativeMapsExample);
